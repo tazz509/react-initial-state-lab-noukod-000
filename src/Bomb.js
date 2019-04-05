91c3591966aca5cd38e3628a11624a6123434e1c
carly <carly@noukod.com>
2019-04-05 16:38:11 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+
+
+  render() {
+    const timeLeft = this.state.secondsLeft;
+    let message;
+    if (timeLeft === 0) {
+      message = 'Boom!'
+    } else {
+      message = `${timeLeft} seconds left before I go boom!`
+    }
+
+
+    return (
+      <h1>{message}</h1>
+    )
+  }
+
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+export default class ImageSlider extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    const currentSlide = this.state.currentSlideIndex
+    return (
+      <h1>I am on slide {currentSlide}</h1>
+    )
+  }
+
+}
